f63579ce9e22c35369333a3f20aeedefb2b66a5a
Umischael <68106026+2umish8@users.noreply.github.com>
2025-09-14 13:48:43 +0200

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,11 +8,13 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    vueDevTools({
+      launchEditor: 'code',
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
